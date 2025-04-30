6c9cf5d7c0d6d380dacd8877d72146919321db30
salilaherath <salilaherath@gmail.com>
2025-04-30 22:14:06 +1000

    7.2P

---
+++ 7_2/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app); // Create HTTP server from app
+const io = require("socket.io")(http); // Pass http server to socket.io
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public
+app.use(express.static("public"));
+
+// Socket connection
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  setInterval(() => {
+    socket.emit("number", parseInt(Math.random() * 10));
+  }, 1000);
+});
+
+// Start server
+http.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
